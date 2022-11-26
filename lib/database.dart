49a03f1cb8ab70c18a02935deb9f3d26157fa113
jariwalabhavesh <jariwalabhavesh30@gmail.com>
2022-11-26 23:58:58 +0530

    Comments & videos

--- lib/database.dart
+++ lib/database.dart
@@ -8,12 +8,18 @@ class DatabaseHelper {
   late Database db;
   Future<void> initDB() async {
     String path = await getDatabasesPath();
+    // Open the database and store the reference.
     db = await openDatabase(
-      join(path, 'person.db'),
+      // Set the path to the database. Note: Using the `join` function from the
+      // `path` package is best practice to ensure the path is correctly
+      // constructed for each platform.
+      join(path, 'personDB.db'),
+      // When the database is first created, create a table to store persons.
       onCreate: (database, version) async {
+        // Run the CREATE TABLE statement on the database.
         await database.execute(
           """
-            CREATE TABLE users (
+            CREATE TABLE persons (
               id INTEGER PRIMARY KEY AUTOINCREMENT, 
               name TEXT NOT NULL,
               city TEXT NOT NULL
@@ -21,25 +27,33 @@ class DatabaseHelper {
           """,
         );
       },
+      // Set the version. This executes the onCreate function and provides a
+      // path to perform database upgrades and downgrades.
       version: 1,
     );
   }
 
   Future<List<Person>> retrieveUsers() async {
-    final List<Map<String, Object?>> queryResult = await db.query('users');
+    // Query the table for all The Persons.
+    final List<Map<String, Object?>> queryResult = await db.query('persons');
     return queryResult.map((e) => Person.fromMap(e)).toList();
   }
 
   Future<int> insertUser(Person person) async {
-    int result = await db.insert('users', person.toMap());
+    // Insert the person into the correct table.
+    // It will return record primary key id
+    // on successful record insertion
+    int result = await db.insert('persons', person.toMap());
     return result;
   }
 
   Future<int> deleteUser(int id) async {
-    print('delete id : $id');
+    // Remove the Person from the database.
     int result = await db.delete(
-      'users',
+      'persons',
+      // Use a `where` clause to delete a specific person.
       where: "id = ?",
+      // Pass the Person's id as a whereArg to prevent SQL injection.
       whereArgs: [id],
     );
     return result;
@@ -47,9 +61,14 @@ class DatabaseHelper {
 
   /// Update the user record at [personId] in sql database.
   Future<int> updateUser(Person person, int personId) async {
-    print('Updating User: ${person.toMap()}');
-    int result = await db
-        .update('users', person.toMap(), where: "id=?", whereArgs: [personId]);
+    // Update the given Person.
+    int result = await db.update(
+      'persons', person.toMap(),
+      // Ensure that the Person has a matching id.
+      where: "id=?",
+      // Pass the person's id as a whereArg to prevent SQL injection.
+      whereArgs: [personId],
+    );
     return result;
   }
 
--- lib/person.dart
+++ lib/person.dart
@@ -5,11 +5,14 @@ class Person {
 
   Person({this.id = 0, required this.name, required this.city});
 
+  // Convert a Person into a Map. The keys must correspond to the names of the
+  // columns in the database.
   Map<String, dynamic> toMap() => {
         "name": name,
         "city": city,
       };
 
+  // Convert the List<Map<String, dynamic> into a List<Person>.
   factory Person.fromMap(Map<String, dynamic> json) => Person(
         id: json["id"],
         name: json["name"],
